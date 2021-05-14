9fd2d86fc33e7127735dafd17d2598d8a7c8b220
SaiManasa Bharathula <saib2418@gmail.com>
2021-05-14 15:14:46 -0700

    updated codee

--- src/cal/Calendar.java
+++ src/cal/Calendar.java
@@ -1,39 +1,39 @@
 package cal;
 
 public class Calendar {
-	private final Model model;
-	private final View view;
-	private final BasicController controller;
+    private final Model model;
+    private final View view;
+    private final BasicController controller;
 
-	public Calendar() {
-		model = new FileModel();
-		view = new ConsoleView();
-		controller = new BasicController(model, view);
-		//gui = new GuiView();
-	}
-	public void show() {
-		controller.show();
-	}
+    public Calendar() {
+        model = new FileModel();
+        view = new ConsoleView();
+        controller = new BasicController(model, view);
+        //gui = new GuiView();
+    }
 
-	public void quit() {
-		controller.quit();
-	}
+    public void show() {
+        controller.show();
+    }
 
-	public void save() {
-		controller.save();
-	}
+    public void quit() {
+        controller.quit();
+    }
 
-	public Controller getController() {
-		return controller;
-	}
+    public void save() {
+        controller.save();
+    }
 
-	public static void main (String [] args) {
-		Calendar calendar = new Calendar();
+    public Controller getController() {
+        return controller;
+    }
 
-		calendar.show();
-		calendar.controller.startAddEvent();
-		calendar.controller.showEvents();
-		calendar.quit();
-	}
+    public static void main(String[] args) {
+        Calendar calendar = new Calendar();
+        calendar.show();
+        calendar.controller.startAddEvent();
+        calendar.controller.showEvents();
+        calendar.quit();
+    }
 }
 
--- src/cal/SimpleCalendarTester.java
+++ src/cal/SimpleCalendarTester.java
@@ -3,11 +3,11 @@ package cal;
 public class SimpleCalendarTester {
 
 
-	public static void main(String[] args) {
-		Calendar calendar = new Calendar();
-		GuiView g = new GuiView();
-		//new Calendar().show();
-		g.setup(calendar.getController()); 
+    public static void main(String[] args) {
+        Calendar calendar = new Calendar();
+        GuiView g = new GuiView();
+        //new Calendar().show();
+        g.setup(calendar.getController());
 
-	}
+    }
 }
